fd6b0091a7b278dba6056dcd5bf2c2544b757711
terry <terry.cadd@gmail.com>
2026-07-07 01:25:45 +0100

    really difficult section 3.2
    like want to add up all the numbers , if none put N 0
    then add up all variables in any order
    
    later adds multiplication to complicate matters

--- concrete-semantics/Chapter3.thy
+++ concrete-semantics/Chapter3.thy
@@ -1,16 +1,12 @@
 
+(* Chapter3.thy *)
+
 theory Chapter3
   imports Main
 begin
 
 type_synonym vname = "string"
 datatype aexp = N int | V vname | Plus aexp aexp
-
-value "N 5" (* represent number 5 *)
-value "V ''x''" (* this is the variable x *)
-value "''x''" (* this is how to write the string x *)
-value "''x'' :: string" (* this is how to write a string x *)
-value "(''x'' :: string) = (''x'' :: char list)" (* string is synonym for char list *)
 
 (* multiple expressions like vname \<Rightarrow> val , we put quotes around it 
 we will see if it causes problems later on *)
@@ -65,112 +61,28 @@ of the form Plus (N i ) (N j ).
 Then prove optimal (asimp_const a).
 *)
 
+(* 
+bothNumbers is a helper routine because cannot pattern match on 
+right hand side of optimal (Plus a1 a2) = if a1 = (N i) & a2 = (N j) then False ...
+so we wrote bothNumbers hopefully get us over this hurdle
+*)
+fun bothNumbers :: "aexp \<Rightarrow> aexp \<Rightarrow> bool" where 
+"bothNumbers (N _)(N _) = True"
+| "bothNumbers _ _ = False" 
+
 fun optimal :: "aexp \<Rightarrow> bool" where
 "optimal (N n)  = True" |
 "optimal (V x ) = True" |
-"optimal (Plus a1 a2) = (case (a1,a2) of 
-    (N i, N j) \<Rightarrow> False 
-    | _ \<Rightarrow> (optimal a1) \<and> (optimal a2))"
+"optimal (Plus a1 a2) = (if bothNumbers a1 a2 then False 
+                       else (optimal a1) & (optimal a2))"
 
-value "N x"
-value "true" (* true is unknown *)
-value "True" (* True has type bool *)
-(* 
- cant do this has wrong type ?
- assume 0: "(N x)" 
+(* & and \<and> seem to be logically equivalent *)
+lemma and1 : " (P & Q) = (P \<and> Q) "
+  by simp
+  
 
-    
-
-    print_facts
-    from 1 show ?case by (rule ssubst)
-
-    (* Now replace in the goal *)
-    from this have 2: "optimal (N x)" by (rule ssubst)
-    from this have 3: "True" by (rule optimal.simps)
-    
-         
-    
-     
-
-    show "optimal (asimp_const (N x))" using 1 by (rule 1)
-
-      using cong
-      apply (rule ssubst)
-      apply (rule optimal.simps)
-
-    assume 0: "optimal (asimp_const (N x))"
-    from 0 have 1: "asimp_const (N x) = (N x) " by (simp add: asimp_const.simps(1))
-    from 1 have 2: "optimal (asimp_const (N x)) = optimal (N x)" by (simp add: 1)
-    from 2 have 3: "optimal (N x) = True" by (simp add: 2)
-    print_facts
-
-(* proof (state)
-goal (1 subgoal):
- 1. optimal (asimp_const (N x)) 
-*)
-    thm asimp_const.simps(1)
-   (* asimp_const (N ?n) = N ?n *)
-
-*)
-
-
-lemma "\<not> surj (f :: 'a \<Rightarrow> 'a set )"
-proof
-assume 0: "surj f"
-from 0 have 1: "\<forall> A. \<exists> a. A = f a" by(simp add: surj_def )
-from 1 have 2: "\<exists> a. {x . x \<notin> f x } = f a" by blast
-from 2 show "False" by blast
-qed
-
-(* 
-fun optimal :: "aexp \<Rightarrow> bool" where
-"optimal (N n)  = True" |
-"optimal (V x ) = True" |
-"optimal (Plus a1 a2) = (case (a1,a2) of 
-    (N i, N j) \<Rightarrow> False 
-    | _ \<Rightarrow> (optimal a1) \<and> (optimal a2))"
-
-fun asimp_const :: "aexp \<Rightarrow> aexp" where
-"asimp_const (N n)  = N n" |
-"asimp_const (V x ) = V x" |
-"asimp_const (Plus a1 a2 ) =
-(case (asimp_const a1, asimp_const a2 ) of
-      (N n1 , N n2 ) \<Rightarrow> N (n1 + n2) |
-      (b1,b2 )       \<Rightarrow> Plus b1 b2 )"
-
-lemma optimal_2 : "optimal (asimp_const v)"
-is a proposition - does simplifying constants asimp_const result in an 
-optimal expression ?
-
-lemma optimal_2 : "optimal (asimp_const v)"
-induction on v 
-v is (N n) then 
-    Prove : optimal (asimp_const (N n))
-    optimal (N n) by asimp_const def
-    True by optimal def
-v is (V x) 
-    Prove : optimal (asimp_const (V x))
-    optimal (V x) by asimp_const def
-    True by optimal def
-v is (Plus a1 a2)
-    Prove : optimal (asimp_const (Plus a1 a2))
-*)
-
-
-(*
-lemma "\<lbrakk> optimal (asimp_const v1) ; optimal (asimp_const v2) \<rbrakk> 
-       \<Longrightarrow> optimal (asimp_const (Plus v1 v2))"
-proof (cases (Pair a b))
-  *)
-
-lemma optimal_3 : "optimal (asimp_const (Plus (N i) (N j)))"
-  unfolding asimp_const.simps
-  apply (simp)
-
- 
-
-lemma optimal_2 : "optimal (asimp_const v)"
-proof (induction v)
+lemma "optimal (asimp_const a)"
+proof(induction a)
   case (N x)
   then show ?case by simp
 next
@@ -178,71 +90,62 @@ next
   then show ?case by simp
 next
   case (Plus a1 a2)
-  then show ?case
-  proof cases
-    case both_nums
-    (* Under this branch, asimp_const unfolds to N (n1 + n2) *)
-    then show ?thesis 
-      by (simp add: asimp_const.simps optimal.simps Plus.IH split: aexp.splits)
-  next
-    case not_both_nums
-    (* Under this branch, it evaluates to Plus b1 b2 where b1,b2 are not both N *)
-    then show ?thesis using Plus.IH
-      by (auto simp add: asimp_const.simps optimal.simps)
-  qed
+  then show ?case by (auto split: aexp.splits) 
 qed
 
- 
 
-  
-
-proof (induction v)
-  case (N x)
-  then show ?case 
-    unfolding asimp_const.simps(1) optimal.simps
-    by (rule TrueI)
-next
-  case (V x)
-  then show ?case 
-    unfolding asimp_const.simps(2) optimal.simps
-    by (rule TrueI)
-next
-  case (Plus v1 v2)
-  then show ?case 
-    apply (simp add: t.splits )
-    
-  qed
-  
-
-
-(*
-
-lemma optimal_2 :" optimal (asimp_const v) "
-proof (induction v)
-  case (N x)
-  then show ?case
-    unfolding asimp_const.simps(1) 
-    unfolding optimal.simps   (* line 122 *)
-    by (rule TrueI)
-next 
-  case (V x)
-  then show ?case
-    unfolding asimp_const.simps
-    unfolding optimal.simps
-    by (rule TrueI)
-next
-  case (Plus v1 v2)
-  then show ?case using Plus.IH by (auto split: aexp.split)
-qed
+(* 
+Exercise 3.2. In this exercise we verify constant folding for aexp where we
+sum up all constants, even if they are not next to each other. 
+For example, Plus (N 1) (Plus (V x ) (N 2)) becomes Plus (V x ) (N 3). 
+This goes beyond asimp.
+Define a function full_asimp :: aexp \<Rightarrow> aexp that sums up all constants and
+prove its correctness: aval (full_asimp a) s = aval a s.
 *)
-(* it is not good news , isabelle build says line 122 
-  Running concrete-semantics ...
-concrete-semantics: theory concrete-semantics.Chapter3
-concrete-semantics FAILED (see also "isabelle build_log -H Error concrete-semantics")
-*** Illegal application of proof command in "state" mode
-*** At command "unfolding" (line 122 of "~/code/isabelle/concrete-semantics/Chapter3.thy")
-Unfinished session(s): concrete-semantics
-*)
+
+(* has constants (N i) *)
+fun hasN :: "aexp \<Rightarrow> bool" where
+"hasN (N _) = True"
+| "hasN (V _) = False" 
+| "hasN (Plus a b) = (hasN a \<or> hasN b)" 
+
+fun totN :: "aexp \<Rightarrow> int" where 
+"totN (N i) = i"
+| "totN (V _) = 0"
+| "totN (Plus a b) = (totN a) + (totN b)"
+
+fun simpAdd :: "aexp \<Rightarrow> aexp \<Rightarrow> aexp" where
+"simpAdd (N i)(N j) = N (i+j)" 
+| "simpAdd _ _ = N 0" (* dummy case ? *)
+
+fun isNum :: "aexp \<Rightarrow> bool" where
+"isNum (N _) = True"
+| "isNum _ = False" 
+
+(* *)
+fun lowN :: "aexp \<Rightarrow> aexp" where
+"lowN (N i) = N i"
+| "lowN (V x) = V x"
+| "lowN (Plus a b) = (if isNum a then b else 
+                          (if isNum b then a
+                           else (let sa = lowN a ;
+                                     sb = lowN b in 
+                               Plus sa sb)))"
+
+value "lowN (N 3)"
+value "lowN (Plus (N 3)(N 4))"
+
+
+fun full_asimp :: "aexp \<Rightarrow> aexp" where
+"full_asimp (N n)  = N n" |
+"full_asimp (V x ) = V x" |
+"full_asimp (Plus a1 a2 ) = (let s1 = full_asimp a1 ; 
+                                 s2 = full_asimp a2 in
+                           if bothNumbers s1 s2 then simpAdd s1 s2
+                           else Plus s1 s2)"
+
+
+
 
 (* end of Chapter3 *)
 end
